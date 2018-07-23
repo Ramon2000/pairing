063e0a5a7a1e9af8de56bd92f345b95439865596
Aleksandr Ishchenko <aleksandr@coinbundle.com>
2018-07-23 13:37:05 +0300

    Poducts and basket

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,17 +12,51 @@ angular.module('myApp.view1', ['ngRoute'])
 
         $scope.basket = [];
         $scope.products = [
-            { name: "Table" },
-            { name: "Chair" }
+            { name: "Table", price: 50 },
+            { name: "Chair" , price: 12 }
         ];
 
-        $scope.addProduct = function(name) {
-            $scope.products.push( {'name': name} );
+        $scope.addProduct = function(name, price) {
+            if (price < 0) {
+                return null;
+            }
+
+            return $scope.products.push( {'name': name, 'price': price} );
         };
 
         $scope.addToBasket = function(product) {
             $scope.basket.push(product);
             console.log($scope.basket);
+        };
+
+        $scope.getPrice = function() {
+            if ($scope.basket.length === 0) {
+                return 0;
+            }
+
+            return $scope.basket.reduce((prev, curr) => {
+                const price = prev.price + curr.price;
+                return {price};
+            })['price'];
+        };
+
+        $scope.getBasketTable = function() {
+            const table = [];
+
+            $scope.basket.forEach((item) => {
+                const alreadyExistItem = table.find((fItem) => {
+                    return fItem.name === item.name;
+                });
+
+                if (alreadyExistItem) {
+                    alreadyExistItem.price += item.price;
+                    alreadyExistItem.count += 1;
+                } else {
+                    table.push(Object.assign({count: 1}, item));
+                }
+            });
+
+            return table;
         };
 
         /** CODING FINISH **/
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -36,6 +36,54 @@ describe('app.view1 module', function() {
             expect(_.find(scope.products, { name: "Table" })).toEqual({ name: "Table" });
         }));
 
+        it('Check product with price and check it', inject(function() {
+            scope.addProduct("foobar", 10);
+            expect(_.find(scope.products, { name: "foobar" })).toEqual({ name: "foobar", price: 10 });
+        }));
+
+        it('Product price shouldn\'t be negative', inject(function() {
+            expect(scope.addProduct("foobar", -10)).toEqual(null);
+        }));
+
+
+        it('getBasketTable', inject(function () {
+            scope.addToBasket({name: "foobar", price: 10});
+
+            expect(scope.getBasketTable()).toEqual([{
+                name: "foobar",
+                count: 1,
+                price: 10
+            }]);
+
+            scope.addToBasket({name: "foobaz", price: 10});
+
+            expect(scope.getBasketTable()).toEqual([
+                {
+                    name: "foobar",
+                    count: 1,
+                    price: 10
+                },
+                {
+                    name: "foobaz",
+                    count: 1,
+                    price: 10
+                }]);
+
+            scope.addToBasket({name: "foobaz", price: 1});
+
+            expect(scope.getBasketTable()).toEqual([
+                {
+                    name: "foobar",
+                    count: 1,
+                    price: 10
+                },
+                {
+                    name: "foobaz",
+                    count: 2,
+                    price: 11
+                }]);
+        }))
+
         /** TESTING START
             JASMINE DOCS: https://jasmine.github.io/2.9/introduction
             LODASH DOCS: https://lodash.com/docs/4.17.5
